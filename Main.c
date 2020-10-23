17dae0cc58de635d282421cb9e530c08338bb3f5
oGabrielArruda <gabrielalvesdearruda@gmail.com>
2020-10-23 16:27:32 -0300

    dez para outra parte inteira

--- Main.c
+++ Main.c
@@ -56,21 +56,69 @@ double algumaParaDez(char numero[MAX], int base)
     return resultado;
 }
 
+char transformarResto(char arrayCaracteres[26], int resto)
+{
+    int index = 0;
+
+    if(resto <= 9)
+        return resto + '0';
+
+    for(index = 0; index < (resto-10); index++) {}
+
+    return arrayCaracteres[index];
+}
+
+
+char* dezParaOutraParteInteira(int numero, int outraBase)
+{
+    char arrayCaracteres[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+    char *restosInvertidos, *armazenamento = (char*)malloc(MAX*sizeof(char));
+    int index, tamanhoDoVetor = 0;
+    int resultadoDaDivisao = numero, resto;
+
+    while(resultadoDaDivisao > 0)
+    {
+        resto = resultadoDaDivisao % outraBase;
+        resultadoDaDivisao = (int)resultadoDaDivisao / outraBase;
+        if(tamanhoDoVetor == MAX)
+            break;
+
+        char charResto = transformarResto(arrayCaracteres, resto);
+        armazenamento[tamanhoDoVetor++] = charResto;
+    }
+
+    restosInvertidos = (char*)malloc(tamanhoDoVetor*sizeof(char));
+    for(index = 0; index < tamanhoDoVetor; index++)
+    {
+        restosInvertidos[index] = armazenamento[tamanhoDoVetor-(index+1)];
+    }
+    free(armazenamento);
+    return restosInvertidos;
+}
+
+char* dezParaOutra(double numero, int outraBase)
+{
+    char* parteInteira = dezParaOutraParteInteira((int) numero, outraBase);
+    // parte fracionaria (fazer)
+    return parteInteira;
+}
+
 
 int main()
 {
     char numeroInicial[MAX];
     unsigned int baseInicial, baseDesejada;
+    double resultado;
 
     fgets(numeroInicial, MAX, stdin);
     scanf("%u", &baseInicial);
     scanf("%u", &baseDesejada);
     fflush(stdin);
 
-    double resultado = algumaParaDez(numeroInicial, baseInicial);
-
-
-    printf("%lf\n", resultado);
+    resultado = algumaParaDez(numeroInicial, baseInicial);
+    char* res = dezParaOutra(resultado, baseDesejada);
+    printf("%s\n", res);
 
 
     return 0;
